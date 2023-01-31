b715b5a47bcc76c8486804500d553116eb700a37
Tran Cong Minh <tc.tranminh@gmail.com>
2023-02-01 01:19:25 +0700

    Fix

--- src/main/java/UI/views/Home.java
+++ src/main/java/UI/views/Home.java
@@ -21,7 +21,7 @@ public class Home extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)fir {
         launch();
     }
 }
